954524c3d81b07b0383f7817075550143b12b984
yg00tw8021051818000keh <yg00tw8021051818000keh@bito.ltd>
2022-10-03 00:39:37 +0800

    first upload

---
+++ yolov5_ros/yolov5/run_logitech_webcam.sh
@@ -0,0 +1,1 @@
+python detect_person.py --source data/images --weights yolov5s.pt --conf 0.25
---
+++ yolov5_ros/yolov5/yolov5_ros/mypytorch/bin/rst2man.py
@@ -0,0 +1,27 @@
+#!/home/yg00tw8021051818000keh/catkin_ws/src/Yolov5_ros/yolov5_ros/yolov5_ros/yolov5/yolov5_ros/mypytorch/bin/python
+
+# Author:
+# Contact: grubert@users.sf.net
+# Copyright: This module has been placed in the public domain.
+
+"""
+man.py
+======
+
+This module provides a simple command line interface that uses the
+man page writer to output from ReStructuredText source.
+"""
+
+import locale
+try:
+    locale.setlocale(locale.LC_ALL, '')
+except:
+    pass
+
+from docutils.core import publish_cmdline, default_description
+from docutils.writers import manpage
+
+description = ("Generates plain unix manual documents.  "
+               + default_description)
+
+publish_cmdline(writer=manpage.Writer(), description=description)
